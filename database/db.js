dbf62948a0e8355ee8323a2ca04ba7cccdbc3bb5
Laith-Grira <lgrira12@gmail.com>
2022-01-07 20:06:39 -0500

    adding mockgoose

--- database/db.js
+++ database/db.js
@@ -1,17 +1,35 @@
 const mongoose = require('mongoose');
+const Mockgoose = require('mockgoose').Mockgoose;
 
+// This URI should be in a .env file
 const DB_URI = 'mongodb+srv://shopify-admin:Admin123@cluster-shopify-invento.f4jdn.mongodb.net/Inventory?retryWrites=true&w=majority';
 
 // This function is used to open the connection from the database
+// If testing is occuring, connect to a mock database
 function open() {
     return new Promise((resolve, reject) => {
-        mongoose
+        if (process.env.NODE_ENV === 'test') {
+            const mockgoose = new Mockgoose(mongoose);
+            mockgoose
+                .prepareStorage()
+                .then(() => {
+                    mongoose
+                        .connect('', { useNewUrlParser: true, useUnifiedTopology: true })
+                        .then((res, err) => {
+                            if (err) return reject(err);
+                            resolve();
+                        })
+                        .catch(err => console.log(err));
+                });
+        } else {
+            mongoose
                 .connect( DB_URI , { useNewUrlParser: true, useUnifiedTopology: true })
                 .then((res, err) => {
                     if (err) return reject(err);
                     resolve();
                 })
                 .catch(err => console.log(err));
+        }
     });
 }
 
