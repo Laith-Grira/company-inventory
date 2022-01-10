9383c30beec14ec54b8a047428b6688bd7544b9a
Laith-Grira <lgrira12@gmail.com>
2022-01-09 19:57:34 -0500

    adding the function handelers

--- client/src/components/CreateItem/CreateItem.js
+++ client/src/components/CreateItem/CreateItem.js
@@ -1,8 +1,42 @@
+import { useState } from "react";
+import axios from 'axios';
 import { Link } from "react-router-dom";
 import './CreateItem.css';
 
 
 const CreateItem = () => {
+
+    // Input values by the user
+    const [nameValue, setNameValue] = useState('');
+    const [priceValue, setPriceValue] = useState(0);
+    const [countValue, setCountValue] = useState(0);
+
+    const handleNameChange = (e) => {
+        setNameValue(e.target.value);
+    }
+
+    const handlePriceChange = (e) => {
+        setPriceValue(e.target.value);
+    } 
+
+    const handleCountChange = (e) => {
+        setCountValue(e.target.value);
+    } 
+
+    const handlePost = async () => {
+        if ((nameValue === '') || (priceValue == 0) || (countValue == 0)) {
+            return;
+        }
+        const item = {
+            name: nameValue,
+            price: priceValue,
+            count: countValue
+        }
+        await axios.post('/items', item)
+                    .then(alert('Item was created successfully! \n You can create a new Item or you can "Go Back"'))
+                    .catch(err => console.log(err));
+    }
+
     return ( 
         <div className="create-main">
             <h1>Create new Item</h1>
@@ -13,24 +47,21 @@ const CreateItem = () => {
                 </div>
                 <div className="form-group">
                     <label htmlFor="item-name">Item Name</label>
-                    <input type="text" className="form-control" id="item-name" placeholder="Item" required/>
+                    <input type="text" className="form-control" name="name" id="item-name" placeholder="Item" onChange={handleNameChange} required/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="item-price">Price</label>
-                    <input type="number" className="form-control" id="item-price" placeholder="Price" min={0.01} step={0.01} required/>
+                    <input type="number" className="form-control" name="price" id="item-price" placeholder="Price" min={0.01} step={0.01} required onChange={handlePriceChange}/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="item-count">Count</label>
-                    <input type="number" className="form-control" id="item-count" placeholder="Count" min={0} required/>
+                    <input type="number" className="form-control" name="count" id="item-count" placeholder="Count" min={0} required onChange={handleCountChange}/>
                 </div>
                 <div className="form-group" >
-                    <button type="submit" className="btn btn-primary">Submit</button>
-                    <Link
-                        to="/">
-                        <button className="btn btn-danger" id="cancel-button">Cancel</button>
-                    </Link>
+                    <button type="submit" className="btn btn-primary" onClick={handlePost}>Submit</button>
+                    <Link to="/" className="btn btn-danger" id="cancel-button">Go Back</Link>
                 </div>
-                </form>
+            </form>
         </div>
      );
 }
